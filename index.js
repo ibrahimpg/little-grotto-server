8978fd92362a274cd7d59b71c01df2219b5b79cb
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2020-05-08 15:59:50 -0500

    fix

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ const submitCandle = require('./controllers/submitCandle');
 const approveCandle = require('./controllers/approveCandle');
 
 app.use(express.urlencoded({ extended: true })); 
-app.use(express.json());
+// app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*' /* process.env.CLIENT_URL */);
+  res.header('Access-Control-Allow-Origin', 'littlegrotto.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   next();
