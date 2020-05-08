ff23629c3e38d7cc567f7892544b31415fd274a5
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2020-05-08 15:56:39 -0500

    bugfix

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ const viewUnapproved = require('./controllers/viewUnapproved');
 const submitCandle = require('./controllers/submitCandle');
 const approveCandle = require('./controllers/approveCandle');
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*' /* process.env.CLIENT_URL */);
