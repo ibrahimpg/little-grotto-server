d6dbcb119ad1e23d893fe6646665c4f41994f9dd
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2020-06-15 09:38:25 -0400

    test

--- controllers/submitCandle.js
+++ controllers/submitCandle.js
@@ -34,23 +34,23 @@ module.exports = (req, res) => {
       https://littlegrotto.com/candles-awaiting-approval
     `,
   })
-    /*.then(() => {
+    .then(() => {
 
-      fetchUrl('https://us12.api.mailchimp.com/3.0/lists/0674bb94a2', {
-        method: 'GET',
+      fetchUrl('https://us12.api.mailchimp.com/3.0/lists/0674bb94a2/members', {
+        method: 'POST',
         headers: {
           'Authorization': process.env.MAILCHIMP_AUTH,
-          // 'Content-Type': 'application/json',
+          'Content-Type': 'application/json',
         },
-        body: {
+        body: [{
           email_address: req.body.candleEmail,
           status: 'subscribed', 
-        },
+        }],
       })
       .then(response => console.log(response))
       .catch(err => console.log(err));
 
-    })*/
+    })
     .then(() => {
       if (/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(req.body.candleFriends[0]) === true) {
           return transporter.sendMail({
