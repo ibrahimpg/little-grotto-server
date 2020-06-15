507c93a8ae7360f580fa53a279f9bcf200bc8591
Ibrahim PG <ibrahim.ghalioum@gmail.com>
2020-06-15 14:52:57 -0400

    test

--- controllers/approveCandle.js
+++ controllers/approveCandle.js
@@ -2,6 +2,9 @@ const database = require('../index');
 
 module.exports = (req, res) => {
   database.db().collection('candles').updateOne({ identifier: req.params.identifier }, { $set: { approved: true } })
+    .then((candle) => {
+      console.log(candle.email);
+    })
     .then(() => res.redirect('https://littlegrotto.com/candles-awaiting-approval/'))
     .catch(err => console.log(err));
 };
